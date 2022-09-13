7936bca269e7976303c698588773ff1e2b3011d8
Eяw!и Gontier <ierwinx@me.com>
2022-09-13 17:09:22 -0500

    Initial Commit

---
+++ Colores/ColoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoresApp.swift
+//  Colores
+//
+//  Created by Erwin Luz León on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ColoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
